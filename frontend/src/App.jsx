77c3f5b38b2cdf0c16ef0cddaffda07d523a22d7
KhoeTrinh <kelvintrinh09@gmail.com>
2024-08-23 17:30:31 +0700

    wqd

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 // import Login from './pages/Login';
-import Signup from './pages/Signup';
+import Home from './pages/home/Home';
+// import Signup from './pages/Signup';
 
 function App() {
     return (
         <div className='p-4 h-screen flex items-center justify-center'>
-            <Signup />
+            <Home />
         </div>
     );
 }
