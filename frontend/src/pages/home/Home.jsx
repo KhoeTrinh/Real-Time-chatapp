18b0abb10cb1e165b85f058669e503fea631b9cf
KhoeTrinh <kelvintrinh09@gmail.com>
2024-08-24 14:16:41 +0700

    123

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,8 +1,11 @@
+import Sidebar from "../../components/Sidebar";
+// import MessageContainer from '../../components/MessageContainer'
+
 const Home = () => {
     return (
         <div className='flex sm:h-[450px] md:h-[550px] overflow-hidden rounded-lg bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
           <Sidebar />
-          <MessageContainer />
+          {/* <MessageContainer /> */}
         </div>
     );
 };
